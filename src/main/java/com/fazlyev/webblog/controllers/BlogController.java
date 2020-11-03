30245ebf03c4ad6acccef11e4a39dbddc51dcbbc
Azat Fazlyev <fazliev14@gmail.com>
2020-11-03 18:39:14 +0300

    Article adding developed

--- src/main/java/com/fazlyev/webblog/controllers/BlogController.java
+++ src/main/java/com/fazlyev/webblog/controllers/BlogController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BlogController {
@@ -21,5 +23,19 @@ public class BlogController {
         Iterable<Post> posts = repository.findAll();
         model.addAttribute("posts", posts);
         return "blog-main";
+    }
+
+    @GetMapping("/blog/add")
+    public String blogAdd(Model model){
+        return "blog-add";
+    }
+
+    @PostMapping("/blog/add")
+    public String blogPostAdd(@RequestParam String title, @RequestParam String anons,
+                              @RequestParam String full_text, Model model){
+        Post post = new Post(title, anons, full_text);
+        post.setId(repository.count()+1);
+        repository.save(post);
+        return "redirect:/blog";
     }
 }
